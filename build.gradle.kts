fd8a521e63e2408b0e1427032f779a20cb0bde20
ciscoo <cisco21c@gmail.com>
2020-10-11 11:03:11 -0500

    Add AssertJ

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ description = "Plugin to generate Java artifacts from WSDL"
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.7.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core:3.17.2")
 }
 
 gradlePlugin {
