b145dcdf5c0d75e1a54254a75a87bc117120a268
ciscoo <cisco21c@gmail.com>
2020-10-11 16:28:07 -0500

    Add tests for plugin

---
+++ src/test/java/io/mateo/cxf/codegen/CxfCodegenPluginTests.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.mateo.cxf.codegen;
+
+import io.mateo.cxf.codegen.wsdl2java.Wsdl2JavaTask;
+import org.assertj.core.api.InstanceOfAssertFactories;
+import org.gradle.api.DefaultTask;
+import org.gradle.api.Project;
+import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.artifacts.Dependency;
+import org.gradle.api.artifacts.ModuleDependency;
+import org.gradle.api.specs.Spec;
+import org.gradle.api.tasks.TaskContainer;
+import org.gradle.testfixtures.ProjectBuilder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CxfCodegenPluginTests {
+
+	private Project project;
+
+	@BeforeEach
+	void setUp() {
+		this.project = ProjectBuilder.builder().build();
+		this.project.getPlugins().apply("io.mateo.cxf-codegen");
+	}
+
+	@Test
+	void extensionCreated() {
+		Object extension = this.project.getExtensions().findByName(CxfCodegenPlugin.CXF_CODEGEN_EXTENSION_NAME);
+
+		assertThat(extension).isNotNull();
+		assertThat(extension).isInstanceOf(CxfCodegenExtension.class);
+	}
+
+	@Test
+	void configurationCreated() {
+		Configuration configuration = this.project.getConfigurations()
+				.findByName(CxfCodegenPlugin.CXF_CODEGEN_CONFIGURATION_NAME);
+
+		assertThat(configuration).isNotNull();
+		assertThat(configuration.isVisible()).isFalse();
+		assertThat(configuration.isCanBeConsumed()).isFalse();
+		assertThat(configuration.isCanBeResolved()).isTrue();
+		assertThat(configuration.getDescription()).isEqualTo("Classpath for CXF Codegen.");
+	}
+
+	@Test
+	void defaultDependencies() {
+		Spec<Dependency> spec = (dependency) -> dependency.getName().equals("cxf-tools-wsdlto-frontend-jaxws");
+		List<String> expectedDependencies = Arrays.asList("cxf-core", "cxf-tools-common", "cxf-tools-wsdlto-core",
+				"cxf-tools-wsdlto-databinding-jaxb", "cxf-tools-wsdlto-frontend-jaxws");
+
+		Configuration configuration = this.project.getConfigurations()
+				.getByName(CxfCodegenPlugin.CXF_CODEGEN_CONFIGURATION_NAME);
+
+		assertThat(configuration.getDependencies()).isNotEmpty();
+		assertThat(configuration.getDependencies()).extracting(Dependency::getName)
+				.containsExactlyElementsOf(expectedDependencies);
+		assertThat(configuration.getDependencies()).extracting(Dependency::getVersion)
+				.allMatch((version) -> version.equals(CxfCodegenPlugin.DEFAULT_CXF_VERSION));
+		assertThat(configuration.getDependencies().matching(spec)).singleElement()
+				.asInstanceOf(InstanceOfAssertFactories.type(ModuleDependency.class)).satisfies((dependency) -> {
+					assertThat(dependency.getExcludeRules()).singleElement()
+							.extracting((excludeRule) -> excludeRule.getGroup() + ":" + excludeRule.getModule())
+							.isEqualTo("org.apache.cxf:cxf-rt-frontend-simple");
+				});
+	}
+
+	@Test
+	void registersTasks(@TempDir File temp) {
+		project.getExtensions().configure(CxfCodegenExtension.class, (cxfCodegen) -> {
+			cxfCodegen.getWsdl2java().register("foo", (foo) -> foo.getWsdl().set(temp));
+			cxfCodegen.getWsdl2java().register("bar", (bar) -> bar.getWsdl().set(temp));
+		});
+		TaskContainer tasks = project.getTasks();
+
+		assertThat(tasks.withType(Wsdl2JavaTask.class)).hasSize(2);
+		assertThat(tasks.findByName(CxfCodegenPlugin.WSDL2JAVA_TASK_NAME)).isNotNull().satisfies((wsdl2java) -> {
+			assertThat(wsdl2java).isInstanceOf(DefaultTask.class);
+			assertThat(wsdl2java.getDependsOn()).hasSize(2);
+		});
+	}
+}
