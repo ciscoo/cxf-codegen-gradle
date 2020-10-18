a892dd89176ef3c3cf18e27e013ec78ea22269da
ciscoo <cisco21c@gmail.com>
2020-10-18 16:00:33 -0500

    Don't apply Spotless to all projects

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,29 +5,31 @@ plugins {
 allprojects {
     group = "io.mateo"
 
-    pluginManager.apply("com.diffplug.spotless")
-
     repositories {
         mavenCentral()
     }
 
-    spotless {
-        kotlinGradle {
-            ktlint()
-            trimTrailingWhitespace()
-            endWithNewline()
+    pluginManager.withPlugin("com.diffplug.spotless") {
+        spotless {
+            kotlinGradle {
+                ktlint()
+                trimTrailingWhitespace()
+                endWithNewline()
+            }
         }
     }
 }
 
 subprojects {
-    spotless {
-        java {
-            eclipse()
-            licenseHeaderFile(rootProject.file("src/spotless/apache-license-2.0.java"), "(package|import|open|module)")
-            removeUnusedImports()
-            trimTrailingWhitespace()
-            endWithNewline()
+    pluginManager.withPlugin("com.diffplug.spotless") {
+        spotless {
+            java {
+                eclipse()
+                licenseHeaderFile(rootProject.file("src/spotless/apache-license-2.0.java"), "(package|import|open|module)")
+                removeUnusedImports()
+                trimTrailingWhitespace()
+                endWithNewline()
+            }
         }
     }
 }
