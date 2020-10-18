75f2bb118d21fc0bdfef626337462ba983d57220
ciscoo <cisco21c@gmail.com>
2020-10-18 10:56:21 -0500

    Define plugin versions in settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,8 @@
+pluginManagement {
+    plugins {
+        id("com.diffplug.spotless") version "5.6.1"
+        id("com.gradle.plugin-publish") version "0.12.0"
+    }
+}
+
 rootProject.name = "cxf-codegen-gradle"
