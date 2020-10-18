131969ea815e0a3b530056c1b067a6ca7f799d98
ciscoo <cisco21c@gmail.com>
2020-10-18 10:34:57 -0500

    Extract Java library conventions to plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,6 @@
-import java.nio.charset.StandardCharsets
-import java.util.Locale
-import org.gradle.api.tasks.testing.logging.TestLogEvent
-
 plugins {
     `java-gradle-plugin`
+    `java-library-conventions`
     `maven-publish`
     id("com.gradle.plugin-publish") version "0.12.0"
     id("com.diffplug.spotless") version "5.6.1"
@@ -16,13 +13,6 @@ repositories {
 group = "io.mateo"
 description = "CXF Codegen Gradle"
 
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.7.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("org.assertj:assertj-core:3.17.2")
-    testImplementation("commons-io:commons-io:2.8.0")
-}
-
 gradlePlugin {
     plugins {
         create("cxfCodegen") {
@@ -31,13 +21,6 @@ gradlePlugin {
             description = "Plugin to generate Java artifacts from WSDL"
             implementationClass = "io.mateo.cxf.codegen.CxfCodegenPlugin"
         }
-    }
-}
-
-java {
-    withSourcesJar()
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(15))
     }
 }
 
@@ -68,6 +51,10 @@ configurations {
     }
 }
 
+dependencies {
+    "functionalTestImplementation"("commons-io:commons-io:2.8.0")
+}
+
 tasks {
     val functionalTest by registering(Test::class) {
         description = "Runs the functional tests."
@@ -80,72 +67,6 @@ tasks {
     }
     publishPlugins {
         dependsOn(build)
-    }
-    withType<JavaCompile>().configureEach {
-        options.encoding = StandardCharsets.UTF_8.toString()
-    }
-
-    // https://docs.oracle.com/en/java/javase/15/docs/specs/man/javac.html
-    compileJava {
-        options.release.set(JavaVersion.VERSION_1_8.majorVersion.toInt())
-        options.compilerArgs.addAll(listOf("-Xlint:all", "-Werror"))
-    }
-    compileTestJava {
-        options.release.set(JavaVersion.VERSION_15.majorVersion.toInt())
-        options.compilerArgs.addAll(listOf("-Xlint", "-Xlint:-overrides", "-Werror", "-parameters"))
-    }
-
-    withType<Jar>().configureEach {
-        from(rootDir) {
-            include("LICENSE.txt")
-            into("META-INF")
-        }
-        manifest {
-            attributes["Automatic-Module-Name"] = project.name.replace("-", ".")
-            attributes["Created-By"] = "${System.getProperty("java.version")} (${System.getProperty("java.vendor")} ${System.getProperty("java.vm.version")})"
-            attributes["Implementation-Title"] = project.description
-            attributes["Implementation-Version"] = project.version.toString()
-            attributes["Specification-Title"] = project.description
-            attributes["Specification-Version"] = project.version.toString()
-        }
-    }
-    javadoc {
-        options {
-            memberLevel = JavadocMemberLevel.PROTECTED
-            header = project.name
-            encoding = StandardCharsets.UTF_8.toString()
-            locale = Locale.ENGLISH.language
-            this as StandardJavadocDocletOptions
-            links = listOf(
-                "https://docs.oracle.com/javase/8/docs/api/",
-                "https://docs.gradle.org/current/javadoc/"
-            )
-            addBooleanOption("Xdoclint:html,syntax", true)
-            addBooleanOption("html5", true)
-            use()
-            noTimestamp()
-            // Suppress warnings due to cross-module @see and @link references.
-            logging.captureStandardError(LogLevel.INFO)
-            logging.captureStandardOutput(LogLevel.INFO)
-        }
-        javadocTool.set(javaToolchains.javadocToolFor {
-            languageVersion.set(JavaLanguageVersion.of(15))
-        })
-    }
-    withType<Test>().configureEach {
-        useJUnitPlatform {
-            includeEngines("junit-jupiter")
-        }
-        testLogging {
-            events = setOf(
-                    TestLogEvent.FAILED,
-                    TestLogEvent.STANDARD_ERROR,
-                    TestLogEvent.SKIPPED
-            )
-        }
-        javaLauncher.set(javaToolchains.launcherFor {
-            languageVersion.set(JavaLanguageVersion.of(15))
-        })
     }
     wrapper {
         distributionType = Wrapper.DistributionType.ALL
---
+++ buildSrc/src/main/kotlin/java-library-conventions.gradle.kts
@@ -0,0 +1,92 @@
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+import java.nio.charset.StandardCharsets
+import java.util.Locale
+
+plugins {
+    `java-library`
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.7.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core:3.17.2")
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(15))
+    }
+}
+
+tasks {
+    withType<Jar>().configureEach {
+        from(rootDir) {
+            include("$rootDir/LICENSE.txt")
+            into("META-INF")
+        }
+        manifest {
+            attributes["Automatic-Module-Name"] = project.name.replace("-", ".")
+            attributes["Created-By"] = "${System.getProperty("java.version")} (${System.getProperty("java.vendor")} ${System.getProperty("java.vm.version")})"
+            attributes["Implementation-Title"] = project.description
+            attributes["Implementation-Version"] = project.version.toString()
+            attributes["Specification-Title"] = project.description
+            attributes["Specification-Version"] = project.version.toString()
+        }
+    }
+
+    withType<JavaCompile>().configureEach {
+        options.encoding = StandardCharsets.UTF_8.toString()
+    }
+
+    withType<Test>().configureEach {
+        useJUnitPlatform {
+            includeEngines("junit-jupiter")
+        }
+        testLogging {
+            events = setOf(
+                TestLogEvent.FAILED,
+                TestLogEvent.STANDARD_ERROR,
+                TestLogEvent.SKIPPED
+            )
+        }
+        javaLauncher.set(javaToolchains.launcherFor {
+            languageVersion.set(JavaLanguageVersion.of(15))
+        })
+    }
+
+    compileJava {
+        options.release.set(JavaVersion.VERSION_1_8.majorVersion.toInt())
+        options.compilerArgs.addAll(listOf("-Xlint:all", "-Werror"))
+    }
+
+    compileTestJava {
+        options.release.set(JavaVersion.VERSION_15.majorVersion.toInt())
+        options.compilerArgs.addAll(listOf("-Xlint", "-Xlint:-overrides", "-Werror", "-parameters"))
+    }
+
+    javadoc {
+        options {
+            memberLevel = JavadocMemberLevel.PROTECTED
+            header = project.name
+            encoding = StandardCharsets.UTF_8.toString()
+            locale = Locale.ENGLISH.language
+            this as StandardJavadocDocletOptions
+            links = listOf(
+                "https://docs.oracle.com/javase/8/docs/api/",
+                "https://docs.gradle.org/current/javadoc/"
+            )
+            addBooleanOption("Xdoclint:html,syntax", true)
+            addBooleanOption("html5", true)
+            use()
+            noTimestamp()
+            // Suppress warnings due to cross-module @see and @link references.
+            logging.captureStandardError(LogLevel.INFO)
+            logging.captureStandardOutput(LogLevel.INFO)
+        }
+        javadocTool.set(javaToolchains.javadocToolFor {
+            languageVersion.set(JavaLanguageVersion.of(15))
+        })
+    }
+}
