493e6e20e7053f2815a3267f57e933d51629cb69
ciscoo <cisco21c@gmail.com>
2020-11-25 10:55:29 -0600

    Upgrade to AssertJ 3.18.1

--- dependencies/build.gradle.kts
+++ dependencies/build.gradle.kts
@@ -9,6 +9,6 @@ javaPlatform {
 
 dependencies {
     api(platform("org.junit:junit-bom:5.7.0"))
-    api("org.assertj:assertj-core:3.17.2")
+    api("org.assertj:assertj-core:3.18.1")
     api("commons-io:commons-io:2.8.0")
 }
