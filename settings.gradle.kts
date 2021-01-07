91613496c7486b9a35625c154d4cd004e3f608f8
ciscoo <cisco21c@gmail.com>
2021-01-06 21:40:14 -0600

    Upgrade to Spotless 5.9.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 pluginManagement {
     plugins {
-        id("com.diffplug.spotless") version "5.8.2"
+        id("com.diffplug.spotless") version "5.9.0"
         id("com.gradle.plugin-publish") version "0.12.0"
         id("org.asciidoctor.jvm.convert") version "3.3.0"
         id("org.ajoberstar.git-publish") version "3.0.0"
