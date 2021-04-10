b6b107fb86f969dfa18c8a186369d10502413b6c
ciscoo <cisco21c@gmail.com>
2021-04-10 10:47:37 -0500

    Support Gradle 7

--- cxf-codegen-gradle/src/functionalTest/java/io/mateo/cxf/codegen/ConfigurationCacheFunctionalTests.java
+++ cxf-codegen-gradle/src/functionalTest/java/io/mateo/cxf/codegen/ConfigurationCacheFunctionalTests.java
@@ -29,7 +29,7 @@ class ConfigurationCacheFunctionalTests {
 
 	@RegisterExtension
 	static GradleCompatibilityExtension gradleCompatibilityExtension = new GradleCompatibilityExtension("6.6.1",
-			"6.7.1", "current", "7.0-rc-2");
+			"6.7.1", "6.8.3", "current");
 
 	@TestTemplate
 	void configurationCache(GradleBuild gradleBuild) {
