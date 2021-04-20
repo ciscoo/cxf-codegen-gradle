dd388800e1eb010e50497cd634aa5bfc223780ec
ciscoo <cisco21c@gmail.com>
2021-04-19 21:36:53 -0500

    Test disable all logs sample
    See gh-5

--- cxf-codegen-gradle/src/functionalTest/java/docs/GeneratingJavaFunctionalTests.java
+++ cxf-codegen-gradle/src/functionalTest/java/docs/GeneratingJavaFunctionalTests.java
@@ -17,13 +17,17 @@ package docs;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import io.mateo.junit.GradleBuild;
 import io.mateo.junit.GradleCompatibilityExtension;
 
+import org.apache.commons.io.FileUtils;
 import org.gradle.testkit.runner.BuildResult;
+import org.gradle.testkit.runner.GradleRunner;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
@@ -38,6 +42,26 @@ class GeneratingJavaFunctionalTests {
 
 		assertThat(result.getOutput()).contains("first markGenerated true").contains("second markGenerated true")
 				.contains("third markGenerated true");
+	}
+
+	@TestTemplate
+	void disableAllLogs(GradleBuild gradleBuild) throws IOException {
+		GradleRunner runner = gradleBuild.script(scriptFor("disable-all-logs")).prepareRunner("wsdl2java");
+		String logback = """
+				<?xml version="1.0" encoding="UTF-8"?>
+				<configuration>
+				</configuration>
+				""";
+		Files.writeString(Path.of(gradleBuild.getProjectDir().getAbsolutePath(), "logback.xml"), logback);
+		FileUtils.moveFile(FileUtils.getFile(gradleBuild.getProjectDir(), "wsdls/calculator.wsdl"),
+				FileUtils.getFile(gradleBuild.getProjectDir(), "example.wsdl"));
+		FileUtils.moveFile(FileUtils.getFile(gradleBuild.getProjectDir(), "wsdls/calculator.xsd"),
+				FileUtils.getFile(gradleBuild.getProjectDir(), "example.xsd"));
+
+		BuildResult result = runner.build();
+
+		assertThat(result.getOutput()).doesNotContain("DEBUG org.apache.cxf.common.logging.LogUtils",
+				"DEBUG org.apache.cxf.tools.wsdlto.core.PluginLoader", "DEBUG org.apache.velocity");
 	}
 
 	String scriptFor(String name) {
--- documentation/src/docs/gradle/generating-java/disable-all-logs.gradle.kts
+++ documentation/src/docs/gradle/generating-java/disable-all-logs.gradle.kts
@@ -1,5 +1,30 @@
 import io.mateo.cxf.codegen.wsdl2java.Wsdl2JavaTask
 
+plugins {
+    java
+    id("io.mateo.cxf-codegen")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    cxfCodegen("ch.qos.logback:logback-classic:1.2.3")
+    cxfCodegen("jakarta.xml.ws:jakarta.xml.ws-api:2.3.3")
+    cxfCodegen("jakarta.annotation:jakarta.annotation-api:1.3.5")
+}
+
+cxfCodegen {
+    wsdl2java {
+        register("example") {
+            wsdl.set(file("example.wsdl"))
+        }
+    }
+}
+
+//tag::code[]
 tasks.withType(Wsdl2JavaTask::class).configureEach {
     jvmArgs = listOf("-Dlogback.configurationFile=logback.xml")
 }
+//end::code[]
