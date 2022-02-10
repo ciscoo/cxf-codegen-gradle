e82ef46260e14e34a82d7ef7f0be01a359673d20
ciscoo <cisco21c@gmail.com>
2022-02-09 20:59:23 -0600

    Upgrade to Apache Commons IO 2.11.0

--- dependencies/build.gradle.kts
+++ dependencies/build.gradle.kts
@@ -10,5 +10,5 @@ javaPlatform {
 dependencies {
     api(platform("org.junit:junit-bom:5.8.2"))
     api("org.assertj:assertj-core:3.22.0")
-    api("commons-io:commons-io:2.8.0")
+    api("commons-io:commons-io:2.11.0")
 }
