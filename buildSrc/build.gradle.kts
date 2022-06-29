8eef0827ae0fe2e2492274bda4dc991c32f7dcda
ciscoo <cisco21c@gmail.com>
2022-06-28 22:55:24 -0500

    Upgrade internal dependencies and plugins

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,6 +7,6 @@ repositories {
 }
 
 dependencies {
-    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.2.2")
-    implementation("io.spring.javaformat:spring-javaformat-gradle-plugin:0.0.31")
+    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.7.2")
+    implementation("io.spring.javaformat:spring-javaformat-gradle-plugin:0.0.34")
 }
