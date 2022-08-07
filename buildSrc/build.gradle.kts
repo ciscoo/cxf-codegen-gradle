5381efd55d0f04edd00da343d8973ce3fff116d2
ciscoo <cisco21c@gmail.com>
2022-08-06 22:10:46 -0500

    Upgrade to Spotless 6.9.0

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,6 +7,6 @@ repositories {
 }
 
 dependencies {
-    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.8.0")
+    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.9.0")
     implementation("io.spring.javaformat:spring-javaformat-gradle-plugin:0.0.34")
 }
