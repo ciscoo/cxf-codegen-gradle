626f3d5810980f34ae0a15548194bfb571117892
ciscoo <cisco21c@gmail.com>
2022-10-15 11:07:39 -0500

    Upgrade to Apache CXF 3.5.4
    Closes gh-44

--- cxf-codegen-gradle/src/main/java/io/mateo/cxf/codegen/CxfCodegenPlugin.java
+++ cxf-codegen-gradle/src/main/java/io/mateo/cxf/codegen/CxfCodegenPlugin.java
@@ -72,7 +72,7 @@ public class CxfCodegenPlugin implements Plugin<Project> {
 	/**
 	 * Update documentation when updating version.
 	 */
-	static final String DEFAULT_CXF_VERSION = "3.5.3";
+	static final String DEFAULT_CXF_VERSION = "3.5.4";
 
 	@SuppressWarnings("deprecation")
 	@Override
