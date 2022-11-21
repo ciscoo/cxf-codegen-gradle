a7f49bc4a711266778f3e1024e90c2c4938af7ce
ciscoo <cisco21c@gmail.com>
2022-11-21 17:56:07 -0600

    Upgrade to JUnit 5.9.1

--- dependencies/build.gradle.kts
+++ dependencies/build.gradle.kts
@@ -8,7 +8,7 @@ javaPlatform {
 }
 
 dependencies {
-    api(platform("org.junit:junit-bom:5.9.0"))
+    api(platform("org.junit:junit-bom:5.9.1"))
     api("org.assertj:assertj-core:3.23.1")
     api("commons-io:commons-io:2.11.0")
 }
