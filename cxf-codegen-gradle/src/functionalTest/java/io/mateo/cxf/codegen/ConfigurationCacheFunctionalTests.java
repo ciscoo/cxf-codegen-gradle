7804b20662c169ec1e664add21d7a3ba166d5b90
ciscoo <cisco21c@gmail.com>
2023-03-04 10:48:13 -0600

    Expand test coverage for configuration cache functional tests

--- cxf-codegen-gradle/src/functionalTest/java/io/mateo/cxf/codegen/ConfigurationCacheFunctionalTests.java
+++ cxf-codegen-gradle/src/functionalTest/java/io/mateo/cxf/codegen/ConfigurationCacheFunctionalTests.java
@@ -29,7 +29,7 @@ class ConfigurationCacheFunctionalTests {
 
 	@RegisterExtension
 	static GradleCompatibilityExtension gradleCompatibilityExtension = new GradleCompatibilityExtension("6.6.1",
-			"6.7.1", "6.8.3", "6.9.3", "7.4.2", "7.5.1", "7.6", "current");
+			"6.7.1", "6.8.3", "6.9.3", "7.0.2", "7.1.1", "7.2", "7.3.3", "7.4.2", "7.5.1", "7.6", "current");
 
 	@TestTemplate
 	void configurationCacheCompatibility(GradleBuild gradleBuild) {
