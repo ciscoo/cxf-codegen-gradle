e09122206da4d111766d7b0ca40dedbe9100002e
ciscoo <cisco21c@gmail.com>
2023-03-25 13:11:24 -0500

    Move more dependencies to catalog

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.17.0")
-    implementation("io.spring.javaformat:spring-javaformat-gradle-plugin:0.0.38")
-    implementation("com.squareup:javapoet:1.13.0")
+    implementation(libs.gradle.spotless)
+    implementation(libs.gradle.springJavaFormat)
+    implementation(libs.javapoet)
 }
--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -4,3 +4,11 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
+}
