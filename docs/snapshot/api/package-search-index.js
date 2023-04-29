6d247b2c25b9908761f638fd6b612bef16d4969b
runner <runner@fv-az812-335>
2023-04-29 16:38:55 +0000

    Generated by gradle-git-publish.

--- docs/snapshot/api/package-search-index.js
+++ docs/snapshot/api/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"io.mateo.cxf.codegen"},{"l":"io.mateo.cxf.codegen.dsl"},{"l":"io.mateo.cxf.codegen.internal"},{"l":"io.mateo.cxf.codegen.wsdl2java"},{"l":"io.mateo.cxf.codegen.wsdl2js"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.mateo.cxf.codegen"},{"l":"io.mateo.cxf.codegen.dsl"},{"l":"io.mateo.cxf.codegen.internal"},{"l":"io.mateo.cxf.codegen.wsdl2java"},{"l":"io.mateo.cxf.codegen.wsdl2js"}];updateSearchResults();
