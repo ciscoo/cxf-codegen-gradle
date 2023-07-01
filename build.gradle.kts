6caf80f7f07b70befab167c9e3b4faa21829a434
ciscoo <cisco21c@gmail.com>
2023-07-01 11:00:14 -0500

    Remove SonarQube integration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,9 @@
 plugins {
     `code-style-conventions`
-    id("org.sonarqube") version "3.3"
 }
 
 defaultTasks("build")
 
 allprojects {
     group = "io.mateo"
-}
-
-sonarqube {
-    properties {
-        property("sonar.projectKey", "ciscoo_cxf-codegen-gradle")
-        property("sonar.organization", "mateoio")
-        property("sonar.host.url", "https://sonarcloud.io")
-    }
 }
