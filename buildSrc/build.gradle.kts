9a7ae3913ff53d55c46dbf31fb5e5a7e6f67cc3a
ciscoo <cisco21c@gmail.com>
2023-11-17 17:22:26 -0600

    Workaround Kotlin Gradle plugin deprecation

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,3 +13,19 @@ dependencies {
     implementation(libs.gradle.springJavaFormat)
     implementation(libs.javapoet)
 }
+
+// TODO: https://youtrack.jetbrains.com/issue/KT-63165
+@Suppress("UNCHECKED_CAST")
+val kotlinCKGPCEClass: Class<DefaultTask> = Class
+    .forName("org.jetbrains.kotlin.gradle.plugin.diagnostics.CheckKotlinGradlePluginConfigurationErrors")
+    as Class<DefaultTask>
+
+tasks.withType(kotlinCKGPCEClass).configureEach {
+    val getBuildServiceProvider: java.lang.reflect.Method = Class
+        .forName("org.jetbrains.kotlin.gradle.plugin.diagnostics.KotlinToolingDiagnosticsCollectorKt")
+        .getDeclaredMethod("getKotlinToolingDiagnosticsCollectorProvider", Project::class.java)
+
+    @Suppress("UNCHECKED_CAST")
+    val buildServiceProvider = getBuildServiceProvider.invoke(null, project) as Provider<BuildService<*>>
+    usesService(buildServiceProvider)
+}
