4cdea50f9bd0d043d457d8d2abf94bf03095767f
ciscoo <cisco21c@gmail.com>
2024-01-27 09:31:05 -0600

    Test against Gradle 8.6-rc-3
    See gh-99

--- cxf-codegen-gradle/src/functionalTest/java/io/mateo/junit/GradleCompatibilityExtension.java
+++ cxf-codegen-gradle/src/functionalTest/java/io/mateo/junit/GradleCompatibilityExtension.java
@@ -36,7 +36,7 @@ import org.junit.jupiter.api.extension.TestTemplateInvocationContextProvider;
  */
 public final class GradleCompatibilityExtension implements TestTemplateInvocationContextProvider {
 
-	private static final List<String> GRADLE_8_VERSIONS = List.of("8.3", "8.4", "current", "8.6-rc-2");
+	private static final List<String> GRADLE_8_VERSIONS = List.of("8.3", "8.4", "current", "8.6-rc-3");
 
 	private static final List<String> DEFAULT_GRADLE_VERSIONS;
 
