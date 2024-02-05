bf0f55d793d4ad1241e0953b06ec6f7520ba9e6f
ciscoo <cisco21c@gmail.com>
2024-02-04 20:55:29 -0600

    Generated by gradle-git-publish.

---
+++ docs/2.3.0/api/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.mateo.cxf.codegen"},{"l":"io.mateo.cxf.codegen.dsl"},{"l":"io.mateo.cxf.codegen.internal"},{"l":"io.mateo.cxf.codegen.wsdl2java"},{"l":"io.mateo.cxf.codegen.wsdl2js"}];updateSearchResults();
