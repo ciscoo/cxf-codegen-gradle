3b80f758ac51b3836827d79286820ac4bb8bc589
ciscoo <cisco21c@gmail.com>
2024-08-21 18:40:41 -0500

    Test against Gradle 8.9
    Closes gh-112

--- cxf-codegen-gradle/src/functionalTest/java/io/mateo/junit/GradleCompatibilityExtension.java
+++ cxf-codegen-gradle/src/functionalTest/java/io/mateo/junit/GradleCompatibilityExtension.java
@@ -37,7 +37,7 @@ public final class GradleCompatibilityExtension implements TestTemplateInvocatio
     private final List<String> gradleVersions;
 
     public GradleCompatibilityExtension() {
-        this.gradleVersions = List.of("8.4", "8.5", "8.6", "8.7", "current", "8.9-rc-2");
+        this.gradleVersions = List.of("8.4", "8.5", "8.6", "8.7", "8.8", "current");
     }
 
     public GradleCompatibilityExtension(String... versions) {
