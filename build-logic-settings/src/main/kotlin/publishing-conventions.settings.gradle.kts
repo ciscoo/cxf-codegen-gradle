56188f07d039168a98dca6f1ab2cdaba15ccdb32
ciscoo <cisco21c@gmail.com>
2026-07-31 08:29:55 -0500

    Sign artifacts for publishing

--- build-logic-settings/src/main/kotlin/publishing-conventions.settings.gradle.kts
+++ build-logic-settings/src/main/kotlin/publishing-conventions.settings.gradle.kts
@@ -10,3 +10,19 @@ nmcpSettings {
         publishingTimeout = validationTimeout
     }
 }
+
+gradle.lifecycle.beforeProject {
+    pluginManager.apply("signing")
+
+    extensions.configure(SigningExtension::class) {
+        val isCIEnvironment = System.getenv("CI")?.toBoolean() ?: false
+        val isSnapshot = project.version.toString().endsWith("SNAPSHOT")
+        isRequired = !(isSnapshot || isCIEnvironment)
+    }
+
+   pluginManager.withPlugin("maven-publish") {
+       extensions.configure(SigningExtension::class) {
+           sign(extensions.getByType<PublishingExtension>().publications)
+       }
+   }
+}
